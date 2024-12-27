87cbd200fb0d1773d201152e06e3cfb6fcf5cafc
LarissaGTI <189912869+LarissaGTI@users.noreply.github.com>
2024-12-27 13:05:24 +0000

    NOVO 2

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -8,7 +8,13 @@
 int main() {
     // Definição das variáveis para armazenar as propriedades das cidades
     // Você pode utilizar o código do primeiro desafio
-
+    int populacao;
+    char estado[50];
+    int ponto_turistico;
+    char cidade[40];
+    char codigo[8];
+    float area;
+    float pib;
     
     // Cadastro das Cartas:
     // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
@@ -20,7 +26,20 @@ int main() {
     // scanf("%s", codigo);
     // 
     // (Repita para cada propriedade)
-
+    printf("Digite a População: \n");
+    scanf("%d",&populacao);
+    printf("Digite o Estado: \n");
+    scanf("%s",&estado);
+    printf("Digite o Número de Pontos Turísticos: \n");
+    scanf("%d",&ponto_turistico);
+    printf("Digite o Nome da Cidade: \n");
+    scanf("%s",&cidade);
+    printf("Digite o Código da Cidade: \n");
+    scanf("%s",&codigo);
+    printf("Digite a Área em Km²: \n");
+    scanf("%f",&area);
+    printf("Digite o PIB: \n");
+    scanf("%f",&pib);
     // Comparação de Cartas:
     // Desenvolva a lógica de comparação entre duas cartas.
     // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
@@ -31,7 +50,21 @@ int main() {
     // } else {
     //     printf("Cidade 2 tem maior população.\n");
     // }
-
+    if ( > ) {
+        printf("Temperatura está alta! \n");
+    }   else {
+        printf("Temperatura está dentro dos parâmetros!\n");
+        }
+    if (umidade > 50){
+        printf("Umidade elevada!\n");
+    }   else {
+        printf("Umidade está dentro dos parâmetros!\n");
+    }
+    if (estoque < estoqueMinimo){
+        printf("Estoque abaixo do mínimo!\n");
+    }   else {
+        printf("Estoque normal!\n");
+    }
     // Exibição dos Resultados:
     // Após realizar as comparações, exiba os resultados para o usuário.
     // Certifique-se de que o sistema mostre claramente qual carta venceu e com base em qual atributo.
